828c87696385d08e11b6996c901b84cf3c6e1292
rustlognet <278407912+rustlognet@users.noreply.github.com>
2026-04-27 14:56:32 +0200

    CSS styles + RSS

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,23 @@
+import { DateTime } from "luxon";
+import pluginRss from "@11ty/eleventy-plugin-rss";
+
 export default function (eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("views/assets/css"); 
-    eleventyConfig.addPassthroughCopy("views/assets/img"); 
+    eleventyConfig.addPassthroughCopy("views/assets/css");
+    eleventyConfig.addPassthroughCopy("views/assets/img");
     eleventyConfig.addPassthroughCopy("views/assets/js");
 
     // SHORTCODES
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    // FILTERS (requires luxon)
+    eleventyConfig.addFilter("date", (dateObj, format = "iiii-LL-dd", locale = "en-GB") => {
+        return DateTime.fromJSDate(dateObj).setLocale(locale).toFormat(format);
+    });
+
+    // Plugins
+    eleventyConfig.addPlugin(pluginRss);
+
 };
 
 export const config = {
--- views/index.md
+++ views/index.md
@@ -1,7 +1,21 @@
 ---
 layout: base
 ---
-...
+<section class="feed">
+{% set navPages = collections.post | reverse %}
+
+{% for item in navPages %}
+
+<div class="feed-item">
+
+<div class="feed-date">{{ item.data.date | date }}</div>
+<div><a href="{{ item.url }}">{{ item.data.title }}</a></div>
+
+</div>
+
+{% endfor %}
+
+</section>
 
 
 
